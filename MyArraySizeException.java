cdd050de2d310769ab56a915068c9356f891dc89
Zlatavu <107855053+Zlatavu@users.noreply.github.com>
2022-06-20 16:27:27 +0300

    Add files via upload

---
+++ MyArraySizeException.java
@@ -0,0 +1,10 @@
+package Lesson2;
+
+public class MyArraySizeException {
+
+    public class MyArraySizeException extends Exception {
+        public MyArraySizeException(String message) {
+            super(message);
+        }
+    }
+}
